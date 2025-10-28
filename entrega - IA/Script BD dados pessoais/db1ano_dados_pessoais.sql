eaac92cde1b4e06788697d680fb306ddf25ed6dd
EnzoChris <enzojoaquim85@gmail.com>
2025-10-28 07:42:11 -0300

    Versão dois entrega IA

--- entrega - IA/Script BD dados pessoais/db1ano_dados_pessoais.sql
+++ entrega - IA/Script BD dados pessoais/db1ano_dados_pessoais.sql
@@ -8,11 +8,11 @@ SHOW TIMEZONE;
 -- Tabela pseudonimizada de Super Administrador
 -- --------------------------------------------------
 CREATE TABLE super_adm_pseudo (
-    id VARCHAR(10) PRIMARY KEY,
+    id VARCHAR(10)  PRIMARY KEY,
     nome_pseudo VARCHAR(100) NOT NULL,
 	cargo_pseudo VARCHAR(50) NOT NULL,
     email_pseudo VARCHAR(100) UNIQUE NOT NULL,
-    senha_hash VARCHAR(100) NOT NULL
+    senha_criptografada VARCHAR(100) NOT NULL
 );
 
 -- --------------------------------------------------
@@ -21,10 +21,12 @@ CREATE TABLE super_adm_pseudo (
 CREATE TABLE usuario_pseudo (
     id VARCHAR(55) PRIMARY KEY,
     fk_fabrica_pseudo VARCHAR(10) NOT NULL,
+    id_gerente_pseudo VARCHAR(10) NOT NULL,
     email_pseudo VARCHAR(100) UNIQUE NOT NULL,
-    senha_hash VARCHAR(100) NOT NULL,
+    senha_criptografada VARCHAR(100) NOT NULL,
     nome_pseudo VARCHAR(100) NOT NULL, 
-    tipo_acesso_pseudo VARCHAR(50) NOT NULL,
+    genero VARCHAR(20) NOT NULL,
+    desc_tipoacesso_pseudo TEXT NOT NULL,
     status_pseudo VARCHAR(50)NOT NULL,
     data_criacao_pseudo VARCHAR(40) NOT NULL,
     data_nascimento_pseudo VARCHAR(15) NOT NULL,
